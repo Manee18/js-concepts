dab76999511031cdf3f04157e1d3a0876fa8d4b7
Manee18 <sk.maneesha2001@gmail.com>
2025-05-28 15:50:26 -0500

    first commit

---
+++ callstack.js
@@ -0,0 +1,26 @@
+function readStory() {
+  console.log("Start reading the story");
+  readChapter1();
+  readChapter2();
+  readChapter3();
+  console.log("Finished reading the story");
+}
+
+function readChapter1() {
+  console.log("Reading Chapter 1: The Beginning");
+}
+
+function readChapter2() {
+  console.log("Reading Chapter 2: The Adventure");
+  readShortStory();
+}
+
+function readShortStory() {
+  console.log("Reading a short story inside Chapter 2");
+}
+
+function readChapter3() {
+  console.log("Reading Chapter 3: The End");
+}
+
+readStory();
